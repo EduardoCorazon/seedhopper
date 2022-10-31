6259375a1c0b6002b57113c4fe8f58afa192e8bd
EduardoCorazon <corazoneduardo@att.net>
2022-10-31 13:47:46 -0500

    created cookies (manual)

--- src/pages/login/register.js
+++ src/pages/login/register.js
@@ -2,6 +2,7 @@ import Navbar from '../../content/navbar/navbar';
 import './Login.css';
 import {useCookies} from "react-cookie";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 
@@ -12,23 +13,28 @@ function Register() {
   //For cookies
   const [cookies, setCookie] = useCookies(["user"]);
   function handleCookie() {
-    setCookie("user", "testaccount", { path: "/"});
+    setCookie("name", name, { path: "/"});
+
   }
+
+
+  //For user credentials
+  const [name, setName] = useState('');
 
   return (
     <>
     <header>
         <Navbar/>
     </header>
-    <h1>testing cookies</h1>{cookies.user && <p>{cookies.user}</p>}
+    <h1>testing cookies</h1>{cookies.name && <p>{cookies.name}</p>}
     <div class="login-page">
       <div class="form">
          <form class="register-form">
           
-            <input type="text" placeholder="name"/>
-            <input type="password" placeholder="password"/>
-            <input type="text" placeholder="email address"/>
-            <button>create</button>
+            <input type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="text" placeholder="email address" />
+            <input type="password" placeholder="password" />
+            <button onClick={handleCookie}>create</button>
             <p class="message">Already registered? <Link to='/login'>Sign In</Link></p>
           </form>
           
